c75a279fd13c6dc13556101809991cbb87f652e6
gmychzh <90561143+gmychzh@users.noreply.github.com>
2021-11-21 13:26:34 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
 let distance = 0
 basic.forever(function () {
     distance = mbit_传感器类.Ultrasonic(DigitalPin.P0, DigitalPin.P1)
-    basic.showString("distance")
+    basic.showString("go")
     if (distance < 10) {
         mbit_电机类.Servo(AnalogPin.P2, 90)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . # . # .
+            # . . . #
+            `)
     } else {
         mbit_电机类.Servo(AnalogPin.P2, 0)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # . # .
+            . # . # .
+            `)
     }
 })
